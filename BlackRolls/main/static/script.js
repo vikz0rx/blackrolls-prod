37101aaf4557334a924835d23b08d2aecc77ef75
Viktor Zagoruyko <vik.zgrk@gmail.com>
2019-03-19 20:43:45 +0500

    Minor changes

--- BlackRolls/main/static/script.js
+++ BlackRolls/main/static/script.js
@@ -3,7 +3,14 @@ $(document).ready(function() {
 });
 
 $(window).scroll(function() {    
-    var scroll = $(window).scrollTop();    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 50) {
+        $('.header-warning').fadeOut('slow');
+    } else {
+        $('.header-warning').fadeIn('slow');
+    }
+
     if (scroll >= 0.4 * $(window).height()) {
         $('footer').fadeIn('slow');
     } else {
