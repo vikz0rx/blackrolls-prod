daf245b7f4d13659c8c0f5b950679a7ed789cd66
Viktor Zagoruyko <vik.zgrk@gmail.com>
2019-04-04 01:52:40 +0500

    Final final finally

--- BlackRolls/main/views.py
+++ BlackRolls/main/views.py
@@ -6,10 +6,12 @@ def index(request):
     sushi_tempura_menu = Food.objects.filter(kind=4)
     sushi_black_menu = Food.objects.filter(kind=5)
     pizza_menu = Food.objects.filter(kind=1)
+    salads_menu = Food.objects.filter(kind=6)
     drinks_menu = Food.objects.filter(kind=3)
 
     context = {
         'pizza': pizza_menu,
+        'salads': salads_menu,
         'sushi': sushi_menu,
         'sushi_tempura': sushi_tempura_menu,
         'sushi_black': sushi_black_menu,
